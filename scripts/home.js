428fec1376d78c51397d71681f3ffb383c644dca
Francinemaaciel <francine_sa@hotmail.com>
2024-11-01 15:08:54 -0300

    ajuste notificacao home e att link png para svg

--- scripts/home.js
+++ scripts/home.js
@@ -1,17 +1,23 @@
-function toggleValue(valueId, iconId) {
-    const valueElement = document.getElementById(valueId);
-    const iconElement = document.getElementById(iconId);
-    
-    if (valueElement.dataset.visible === "true") {
-        // Esconder o valor (transforma em asteriscos) e muda o ícone para olho fechado
-        valueElement.textContent = "****";
-        iconElement.classList.replace("bi-eye-fill", "bi-eye-slash-fill");
-        valueElement.dataset.visible = "false";
-    } else {
-        // Mostrar o valor real e muda o ícone para olho aberto
-        valueElement.textContent = valueElement.dataset.value; // Usa o valor armazenado
-        iconElement.classList.replace("bi-eye-slash-fill", "bi-eye-fill");
-        valueElement.dataset.visible = "true";
-    }
+// Abre aba de notificação
+function notificacoes() {
+    const painel = document.getElementById("notificacaoPainel");
+    painel.style.display = painel.style.display === "block" ? "none" : "block";
 }
-  
+
+function toggleValue(valueId, iconId) {
+  const valueElement = document.getElementById(valueId);
+  const iconElement = document.getElementById(iconId);
+
+  if (valueElement.dataset.visible === "true") {
+    // Esconder o valor (transforma em asteriscos) e muda o ícone para olho fechado
+    valueElement.textContent = "****";
+    iconElement.classList.replace("bi-eye-fill", "bi-eye-slash-fill");
+    valueElement.dataset.visible = "false";
+  } else {
+    // Mostrar o valor real e muda o ícone para olho aberto
+    valueElement.textContent = valueElement.dataset.value; // Usa o valor armazenado
+    iconElement.classList.replace("bi-eye-slash-fill", "bi-eye-fill");
+    valueElement.dataset.visible = "true";
+  }
+}
+
